e2f126a656f5ff964a312c22bfbfcb497b61a82c
Jamy Golden <code@jamygolden.com>
2024-06-11 12:44:11 +0200

    Add ribboncurls

---
+++ Formula/ribboncurls.rb
@@ -0,0 +1,17 @@
+class Tinty < Formula
+  desc "Mustache template rendering engine"
+  homepage "https://github.com/tinted-theming/ribboncurls"
+  url ""
+  sha256 ""
+  license "APACHE or MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    assert_match "ribboncurls #{version}", shell_output("#{bin}/ribboncurls --version")
+  end
+end
